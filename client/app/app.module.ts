210a13aa29a9bcdb4df64ac5dda0e8548b2bf49a
Marcel Savegnago <marcel.savegnago@gmail.com>
2017-05-04 00:51:01 -0300

    Aula 2

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FotoModule } from './foto/foto.module';
 
 @NgModule({
 
-    imports: [ BrowserModule ],
+    imports: [ BrowserModule, FotoModule ],
     declarations: [ AppComponent ],
     bootstrap: [ AppComponent ]
 
 })
 
-export class AppModule {
-
-}
+export class AppModule { }

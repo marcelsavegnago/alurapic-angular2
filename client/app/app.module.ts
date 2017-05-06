be4ee6fe2a586197cc221876232b6415bf124dc9
Marcel Savegnago <marcel.savegnago@gmail.com>
2017-05-06 16:11:03 -0300

    Aula 3

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { AppComponent }   from './app.component';
 import { FotoModule } from './foto/foto.module';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
-
-    imports: [ BrowserModule, FotoModule ],
-    declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ]
-
+  imports:      [ BrowserModule, FotoModule, HttpModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
-
 export class AppModule { }
--- client/app/foto/foto.module.ts
+++ client/app/foto/foto.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { FotoComponent } from './foto.component';
 
 @NgModule({
-    declarations: [ FotoComponent ],
-    exports: [ FotoComponent ]
+  declarations: [ FotoComponent ],
+  exports: [FotoComponent]
 })
-
 export class FotoModule { }

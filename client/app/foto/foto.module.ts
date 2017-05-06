fd2835957c5c077d1cedffdf11c76edcf14b361e
Marcel Savegnago <marcel.savegnago@gmail.com>
2017-05-06 17:13:52 -0300

    Aula 04

--- client/app/foto/foto.module.ts
+++ client/app/foto/foto.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { FotoComponent } from './foto.component';
 
 @NgModule({
+  imports: [ CommonModule ],
   declarations: [ FotoComponent ],
   exports: [FotoComponent]
 })
